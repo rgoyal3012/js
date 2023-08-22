0ef2cad21f051251cd379c0c431b04db585213ed
rgoyal3012 <142869593+rgoyal3012@users.noreply.github.com>
2023-08-22 06:11:41 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,6 @@
+const accountId=20;
+let accountEmail="ritesh3012@gmail.com";
+var password="Ritesh@123goyal";
+accountCity="Jaipur";
+
+console.table([accountEmail , accountId , password ,accountCity])
