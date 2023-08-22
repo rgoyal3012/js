c7150ac31e03002d52c725050d67dcbb316e08f7
rgoyal3012 <142869593+rgoyal3012@users.noreply.github.com>
2023-08-22 06:59:04 +0000

    Conversion of datatype into number

---
+++ 01_basics/conversion.js
@@ -0,0 +1,13 @@
+let score="33asdfghjkl";
+
+console.log(typeof score);
+
+let valInNumber=Number(score);
+console.log(typeof valInNumber);
+
+console.log(valInNumber); // NaN will output
+
+let num;
+
+let bool=Boolean(num);
+console.log(bool);  
